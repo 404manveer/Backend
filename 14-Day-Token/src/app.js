f45af9d0d477e05b8e3a45492bc4e5b616dbaa99
Shah097 <your-email@example.com>
2025-08-16 20:58:10 +0530

    14-day-compelet

--- 14-Day-Token/src/app.js
+++ 14-Day-Token/src/app.js
@@ -1,10 +1,12 @@
 
 const exprees = require('express');
 const authrouter = require("./routes/auth.routes")
+const cookieParser = require('cookie-parser');
 const app = exprees();
 
 
 app.use(exprees.json());
+app.use(cookieParser());
 app.use("/auth", authrouter);
 
 
--- 14-Day-Token/src/routes/auth.routes.js
+++ 14-Day-Token/src/routes/auth.routes.js
@@ -1,4 +1,5 @@
 const exprees = require('express');
+const jwt = require('jsonwebtoken');
 const userModel = require('../models/user.model');   
 
 const router = exprees.Router();
@@ -9,6 +10,11 @@ router.post("/register", async (req,res)=>{
         user: user,
         password: password
     });
+    const token = jwt.sign({
+        id: users._id,
+    }, process.env.JWT_SECRET,)
+
+res.cookie("token",token);
 
     if(!user){
         return res.status(400).json({
@@ -17,9 +23,77 @@ router.post("/register", async (req,res)=>{
     }else{
         return res.status(201).json({
             msg: "User registered successfully",
-            user: users
+            user: users,
+        
         }); 
     }
 })
+
+router.get('/login',async (req, res) => {
+    const {user,password} = req.body;
+
+    const users = await userModel.findOne({user: user,  });
+    if(!users){
+        return res.status(401).json({
+            msg: "User not found"
+        });
+    }
+
+    const isPasswordValid = users.password === password;
+    if(!isPasswordValid){  
+        return res.status(401).json({
+            msg: "Invalid password"
+        });
+    }
+    const token = jwt.sign({
+        id: users._id,
+    }, process.env.JWT_SECRET,);
+    res.cookie("token", token);
+
+    res.status(200).json({
+        msg: "User logged in successfully",
+        user: users,
+    });
+})
+
+router.get('/logout', (req, res) => {
+    res.clearCookie("token");  
+   return res.status(200).json({
+        msg: "User logged out successfully"
+    }); 
+});
+
+router.get('/user', async (req, res) => {
+    const {token} = req.cookies;
+    if(!token){
+        return res.status(401).json({
+            msg: "Unauthorized"
+        });
+    }
+
+    try{
+        const decoded  = jwt.verify(token,process.env.JWT_SECRET);
+        const user = await userModel.findById({
+            _id:decoded.id
+        }).select("-password -__v"); // Exclude password and __v field
+        if(!user){
+            return res.status(404).json({
+                msg: "User not found"
+            });
+        }
+        return res.status(200).json({
+            msg: "User found",
+            user: user
+        })
+    }catch(err){
+        return res.status(401).json({
+            msg: "Invalid token"
+        });
+    }
+
+   
+})
+
+
 
 module.exports = router;
